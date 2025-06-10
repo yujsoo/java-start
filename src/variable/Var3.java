8132c89b91e8a65842e0b1705ae64ae3374212ce
유지수 <jsyu333@gmail.com>
2025-06-10 20:03:55 +0900

    chore: 변수 값 변경

---
+++ src/variable/Var3.java
@@ -0,0 +1,12 @@
+package variable;
+
+public class Var3 {
+    public static void main(String[] args) {
+        int a;
+        a = 10; // 변수 초기화
+        System.out.println(a); // 10
+
+        a = 50; // 변수 값 변경
+        System.out.println(a); // 50
+    }
+}
